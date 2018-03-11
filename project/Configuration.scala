c7ea110690e148ce1d29d55055485ebf90181536
Jordi Puig <puig@designweb.es>
2018-03-11 16:29:15 +0100

    server config

--- project/Configuration.scala
+++ project/Configuration.scala
@@ -7,8 +7,11 @@ object Configuration {
     scalaVersion := "2.12.4",
 
     // Custom folders path (/src/main/scala and /src/test/scala by default)
+    mainClass in Compile := Some("tv.codely.api.ScalaHttpApi"),
+    scalaSource in Compile := baseDirectory.value / "/src/main",
     scalaSource in Compile := baseDirectory.value / "/src/main",
     scalaSource in Test := baseDirectory.value / "/src/test",
+    resourceDirectory in Compile := baseDirectory.value / "conf",
 
     // Compiler options
     scalacOptions ++= Seq(
--- src/test/tv/codely/api/ScalaHttpApiTest.scala
+++ src/test/tv/codely/api/ScalaHttpApiTest.scala
@@ -4,21 +4,14 @@ import akka.http.scaladsl.model._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{Matchers, WordSpec}
-import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 
 final class ScalaHttpApiTest extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
-  private val routesWithDefinedResponses =
-    get {
-      path("status") {
-        complete(HttpEntity(ContentTypes.`application/json`, """{"status":"ok"}"""))
-      }
-    }
 
   "ScalaHttpApi" should {
 
     "respond successfully while requesting its status" in {
-      Get("/status") ~> routesWithDefinedResponses ~> check {
+      Get("/status") ~> Routes.all ~> check {
         status shouldBe StatusCodes.OK
         contentType shouldBe ContentTypes.`application/json`
         entityAs[String] shouldBe """{"status":"ok"}"""
@@ -26,7 +19,7 @@ final class ScalaHttpApiTest extends WordSpec with Matchers with ScalaFutures wi
     }
 
     "return a MethodNotAllowed error for PUT requests to the root path" in {
-      Put() ~> Route.seal(routesWithDefinedResponses) ~> check {
+      Put() ~> Route.seal(Routes.all) ~> check {
         status shouldEqual StatusCodes.MethodNotAllowed
         responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
       }
